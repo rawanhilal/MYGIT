c819d7a7ba23f7d4d63f756124408b2c00432643
rawanhilal <80214290+rawanhilal@users.noreply.github.com>
2021-04-14 00:47:11 -0700

    Add files via upload

---
+++ PyLib3/index31.py
@@ -0,0 +1,10 @@
+import math
+print("program to print the greater of two number")
+num1=5
+num2=2
+if num1>b:
+ print('num1 is greater than num2')
+else:
+ print('num1 is greater than num2')
+ print (result)
+
